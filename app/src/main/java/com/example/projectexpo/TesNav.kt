bcf11cc70e3a3e0fcde44bdc815e3efdc0b62c11
Vahri Maulana <vhrimlna@gmail.com>
2023-11-11 21:12:42 +0700

    November 11 2023 9:12

---
+++ app/src/main/java/com/example/projectexpo/TesNav.kt
@@ -0,0 +1,11 @@
+package com.example.projectexpo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TesNav : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tes_nav)
+    }
+}
