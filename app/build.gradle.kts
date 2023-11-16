67ba50ab9b76345421c6ff289df3acea5be23080
Vahri Maulana <vhrimlna@gmail.com>
2023-11-16 08:29:48 +0700

    November 11 2023 9:12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -41,8 +42,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("com.google.android.material:material:1.6.0")
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.4.31")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("nl.joery.animatedbottombar:library:1.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/projectexpo/MainActivity.kt
+++ app/src/main/java/com/example/projectexpo/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.projectexpo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import nl.joery.animatedbottombar.AnimatedBottomBar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomBar: AnimatedBottomBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomBar=findViewById(R.id.bottom_bar)
+
+        replaceFragment(TodayFragment())
+
+        bottomBar.setOnTabSelectListener(object :AnimatedBottomBar.OnTabSelectListener{
+            override fun onTabSelected(
+                lastIndex: Int,
+                lastTab: AnimatedBottomBar.Tab?,
+                newIndex: Int,
+                newTab: AnimatedBottomBar.Tab
+            ) {
+
+                when(newIndex){
+                    0 -> replaceFragment(TodayFragment())
+                    1 -> replaceFragment(ActivityFragment())
+                    2 -> replaceFragment(ArticleFragment())
+                    3 -> replaceFragment(MedicationFragment())
+                    4 -> replaceFragment(ProfileFragment())
+
+                    else -> {
+
+                    }
+                }
+            }
+
+        })
+
+    }
+
+    fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.fragments,fragment).commit()
     }
 }
